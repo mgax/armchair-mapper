7a3da64f5d6170271cae1d4c98cdc22305de44ca
Alex Morega <alex@grep.ro>
2015-08-17 16:54:45 +0300

    serve map file

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+
+var proj = process.env.PROJECT || '/tmp';
 var app = express();
+
+app.get('/map.json', function (req, res) {
+  var map = fs.readFileSync(proj + '/map.json');
+  res.send(map);
+});
 
 app.use(express.static('www'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(+(process.env.PORT || 3000), function () {
   var addr = server.address();
   console.log('Listening at http://%s:%s', addr.address, addr.port);
 });
